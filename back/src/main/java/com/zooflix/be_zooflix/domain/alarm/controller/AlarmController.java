7df8e0e2dcdbc228858f1812f1f181ba5bdd6882
이수민 <judy3504@naver.com>
2024-03-07 17:16:58 +0900

    feat: entityã작성중

--- back/src/main/java/com/zooflix/be_zooflix/domain/alarm/controller/AlarmController.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/alarm/controller/AlarmController.java
@@ -1,4 +1,38 @@
 package com.zooflix.be_zooflix.domain.alarm.controller;
 
+import com.zooflix.be_zooflix.domain.alarm.service.AlarmService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/alarm")
+@CrossOrigin("*")
 public class AlarmController {
+
+    private final AlarmService alarmService;
+
+    public AlarmService getAlarmService() {
+        return alarmService;
+    }
+
+    /*
+     * 7.1 알림을 위한 SSE 연결
+     * */
+
+    /*
+     * 7.2 알림 전체 조회
+     * */
+
+    /*
+     * 7.3 알람 읽음 여부 수정
+     * */
+
+    /*
+     * 7.4 알림 전체 지우기
+     * */
+
+
 }
