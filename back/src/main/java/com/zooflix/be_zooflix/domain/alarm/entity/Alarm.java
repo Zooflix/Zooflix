296d57569c6782bdb07283e663e64a9fb811f1a5
이수민 <judy3504@naver.com>
2024-03-08 15:04:07 +0900

    feat: sse연결요청코ë기능

--- back/src/main/java/com/zooflix/be_zooflix/domain/alarm/entity/Alarm.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/alarm/entity/Alarm.java
@@ -1,16 +1,15 @@
 package com.zooflix.be_zooflix.domain.alarm.entity;
 
 import com.zooflix.be_zooflix.domain.user.entity.User;
+import com.zooflix.be_zooflix.global.entity.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
-
-import java.time.DateTimeException;
 import java.util.Date;
 
 @Entity
 @Getter
 @Table(name = "alarm")
-public class Alarm {
+public class Alarm extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,12 +17,12 @@ public class Alarm {
     private int alarmNo; //알람 id
 
     @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "user_no")
-    @Column(nullable = false)
-    private User receiverUser; //사용자 -> receiver
+    @JoinColumn(name = "user_no", nullable = false)
+    private User receiverUser; //알림을 받는 사용자 -> receiver
 
-    @Column(name = "subscribe_id")
-    private User senderUser; //내가 구독한 사람의 id -> sender
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "subscribe_id")
+    private User senderUser; //내가 구독한 사람의 id(알림을 보내는 사람) -> sender
 
     @Enumerated(EnumType.STRING)
     @Column(name = "alarm_type", nullable = false)
@@ -39,5 +38,15 @@ public class Alarm {
     private Boolean isRead; // 알림 확인 여부
 
 
+    public static Alarm createAlarm(User receiverUser, User senderUser, AlarmTypeStatus alarmType, String content, Boolean isRead) {
+        Alarm alarm = new Alarm();
+
+        alarm.senderUser = senderUser;
+        alarm.receiverUser = receiverUser;
+        alarm.alarmType = alarmType;
+        alarm.isRead = isRead;
+
+        return alarm;
+    }
 
 }
