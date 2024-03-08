0e4af4421e1f9498f2d6f4d5d99108530a6147df
hyejinKim1 <201802084@o.cnu.ac.kr>
2024-03-08 15:04:28 +0900

    feat: 주식구독 entity, controller 생성

--- back/src/main/java/com/zooflix/be_zooflix/domain/stockSubscribe/dto/StockSubscribeDto.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/stockSubscribe/dto/StockSubscribeDto.java
@@ -1,4 +1,17 @@
 package com.zooflix.be_zooflix.domain.stockSubscribe.dto;
+import lombok.*;
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class StockSubscribeDto {
+    private String stockName;
+    private int stockCount;
+    private String subscribeDate;
+    private int userNo;
+    private String userAppKey;
+    private String userSecretKey;
+    private String userAccount;
 }
