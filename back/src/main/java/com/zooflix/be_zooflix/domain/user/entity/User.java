b13a5a82c490a043fbb144e9a79a0965bfc86bb7
김진우 <lkk7209@naver.com>
2024-03-08 13:21:20 +0900

    feat: user entity 추가, docker 작업중

--- back/src/main/java/com/zooflix/be_zooflix/domain/user/entity/User.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/user/entity/User.java
@@ -1,4 +1,48 @@
 package com.zooflix.be_zooflix.domain.user.entity;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userNo;
+    @Column(unique = true)
+    private String userId;
+    @Column
+    private String userPw;
+    @Column(unique = true)
+    private String userName;
+    @Column
+    private String refreshToken;
+    @Column
+    private int reportCount = 0;
+    @Column
+    private LocalDateTime reportDate;
+    @Column
+    private int predictCount = 0;
+    @Column
+    private int successCount = 0;
+    @Column
+    private int failCount = 0;
+    @Column
+    private double userTemperature = 30.0;
+    @Column
+    private String userZbti;
+    @Column
+    private int successStreak = 0;
+    @CreatedDate
+    @Column
+    private LocalDateTime userCreate;
+    @Column
+    private String userAppKey;
+    @Column
+    private String userSecretKey;
+    @Column
+    private String userAccount;
+
 }
