ed830f3a4ef3bd78e3c8c717261a1c50731e4933
양성주 <ztjdwnz@naver.com>
2024-03-08 10:13:34 +0900

    feat: predict

--- back/src/main/java/com/zooflix/be_zooflix/domain/predict/repository/PredictRepository.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/predict/repository/PredictRepository.java
@@ -1,4 +1,11 @@
 package com.zooflix.be_zooflix.domain.predict.repository;
 
-public class PredictRepository {
+import com.zooflix.be_zooflix.domain.predict.entity.Predict;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PredictRepository extends JpaRepository<Predict, Integer> {
+
+    List<Predict> findeByStockName(String stockName);
 }
