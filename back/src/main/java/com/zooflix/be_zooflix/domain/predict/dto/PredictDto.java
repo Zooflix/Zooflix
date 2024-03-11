054301e672c569043e3a2cae297f0614c3cdbc53
양성주 <ztjdwnz@naver.com>
2024-03-11 14:36:54 +0900

    feat: predictDto

--- back/src/main/java/com/zooflix/be_zooflix/domain/predict/dto/PredictDto.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/predict/dto/PredictDto.java
@@ -1,4 +1,36 @@
 package com.zooflix.be_zooflix.domain.predict.dto;
 
+import com.zooflix.be_zooflix.domain.predict.entity.Predict;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class PredictDto {
+    private String stockName;
+    private int userNo;
+    private LocalDate createDate;
+    private LocalDate pdDate;
+    private int pdValue;
+    private String pdContent;
+    private int preValue;
+    private boolean pdUpDown;
+
+    public Predict toEntity() {
+        return Predict.builder()
+                .stockName(stockName)
+                .userNo(userNo)
+                .createDate(LocalDate.now())
+                .pdDate(pdDate)
+                .pdValue(pdValue)
+                .pdContent(pdContent)
+                .preValue(preValue)
+                .pdUpDown(pdUpDown)
+                .build();
+    }
 }
