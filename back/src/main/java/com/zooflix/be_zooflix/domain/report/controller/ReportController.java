73e80a16de3e0b8aaf41e6e608db76477fe12dd0
이수민 <judy3504@naver.com>
2024-03-12 15:10:07 +0900

    feat: report 기능 수정중

--- back/src/main/java/com/zooflix/be_zooflix/domain/report/controller/ReportController.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/report/controller/ReportController.java
@@ -1,4 +1,28 @@
 package com.zooflix.be_zooflix.domain.report.controller;
 
+import com.zooflix.be_zooflix.domain.report.service.ReportService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ReportController {
+
+    private final ReportService reportService;
+
+    public ReportController(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @PostMapping("/predict/report/{userId}/{pdNo}")
+    public ResponseEntity<String> reportPd(@PathVariable("user_id")String userId, @PathVariable("pd_no")int pdNo){
+        try{
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("댓글 신고에 실패했습니다.");
+        }
+    }
 }
