5a367ad872dab6e65a5b64c10544f110e1304619
hyejinKim1 <201802084@o.cnu.ac.kr>
2024-03-12 15:33:18 +0900

    feat: user api key 확인

--- back/src/main/java/com/zooflix/be_zooflix/domain/stockSubscribe/controller/StockSubscribeController.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/stockSubscribe/controller/StockSubscribeController.java
@@ -1,6 +1,7 @@
 package com.zooflix.be_zooflix.domain.stockSubscribe.controller;
 
 import com.zooflix.be_zooflix.domain.stockSubscribe.dto.StockSubscribeDto;
+import com.zooflix.be_zooflix.domain.user.dto.UserKeyProjection;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -56,6 +57,17 @@ public class StockSubscribeController {
     public ResponseEntity<ResultResponse<List<StockSubscribeDto>>> subscribeList(@PathVariable(name = "userId") String userId) {
         List<StockSubscribeDto> subscribes = service.subscribeList(userId);
         return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString(), subscribes));
+    }
+
+    /**
+     * 3.3 구독중인 주식 조회
+     *
+     */
+    @GetMapping("/subscribe/checkApiKey/{userNo}")
+    @Operation(summary = "등록된 API key가 있는지 확인")
+    public ResponseEntity<ResultResponse<UserKeyProjection>> checkUserApiKey(@PathVariable(name = "userNo") int userNo) {
+        UserKeyProjection checkApiKey = service.checkApiKey(userNo);
+        return ResponseEntity.ok(ResultResponse.res(HttpStatus.OK, HttpStatus.OK.toString(), checkApiKey));
     }
 
 }
