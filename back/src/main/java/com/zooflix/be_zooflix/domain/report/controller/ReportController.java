178c9a860c4a996b43db044eeaa1ef6928c6fbbb
이수민 <judy3504@naver.com>
2024-03-12 15:32:07 +0900

    chore: 자잘한 문법 수정

--- back/src/main/java/com/zooflix/be_zooflix/domain/report/controller/ReportController.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/report/controller/ReportController.java
@@ -1,7 +1,6 @@
 package com.zooflix.be_zooflix.domain.report.controller;
 
 import com.zooflix.be_zooflix.domain.predict.dto.PredictResDto;
-import com.zooflix.be_zooflix.domain.report.dto.ReportDto;
 import com.zooflix.be_zooflix.domain.report.service.ReportService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,11 +27,13 @@ public class ReportController {
             PredictResDto reportPdDto = reportService.reportPredict(pdNo, reportReasonString, userId);
 
             if(reportPdDto != null){
-                return ResponseEntity.ok("댓글이 성공적으로 신고되었습니다!");
+                return ResponseEntity.ok("게시글 성공적으로 신고되었습니다!");
+            }else{
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("게시글 신고에 실패했습니다.");
             }
         }catch (Exception e){
             e.printStackTrace();
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("댓글 신고에 실패했습니다.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("게시글 신고에 실패했습니다.");
         }
     }
 }
