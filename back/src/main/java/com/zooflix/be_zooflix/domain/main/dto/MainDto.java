486190258dccbf3276c6ab958a1ed2091f1799d9
hyejinKim1 <201802084@o.cnu.ac.kr>
2024-03-15 15:32:28 +0900

    feat: main 랭킹 데이터 조회 api

--- back/src/main/java/com/zooflix/be_zooflix/domain/main/dto/MainDto.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/main/dto/MainDto.java
@@ -1,4 +1,21 @@
 package com.zooflix.be_zooflix.domain.main.dto;
 
+import com.zooflix.be_zooflix.domain.stockSubscribe.dto.StockRankingDto;
+import com.zooflix.be_zooflix.domain.user.dto.UserRankingDto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MainDto {
+    private List<UserRankingDto> userRankList;
+    private List<StockRankingDto> stockRankingList;
+    private UserRankingDto mostPredictUser;
+    private UserRankingDto mostWrongPredictUser;
 }
