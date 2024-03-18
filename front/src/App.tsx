375704aaea1c3b40a4aaa84f3281f2a102649b64
조민준 <cjmg1085@naver.com>
2024-03-18 17:50:23 +0900

    design: my-page 시작

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/User/Login";
 import Signup from "./pages/User/Signup";
+import Mypage from "./pages/Mypage/Mypage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/my-page" element={<Mypage />} />
       </Routes>
     </div>
   );
--- front/src/pages/Mypage/Mypage.tsx
+++ front/src/pages/Mypage/Mypage.tsx
@@ -1,7 +1,23 @@
 import styled from "styled-components";
+import HeaderLogo from "../../assets/img/Logo.svg";
 
 function Mypage() {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <Container>
+        <img src={HeaderLogo} className="header" alt="HeaderLogo"/>
+      </Container>
+    </Wrapper>
+  )
 }
 
 export default Mypage;
+
+const Wrapper = styled.div``;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
