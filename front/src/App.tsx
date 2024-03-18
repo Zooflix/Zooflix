da499a51a1a71bcb953b37cd0da1ffec91cf283b
s_971001 <s_971001@naver.com>
2024-03-19 00:09:10 +0900

    design:Title.tsx, SquareBtn.tsx, PlayBtn.tsx 생성. App.tsx에 /radio 라우터 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/User/Login";
+import Radio from "./pages/Radio/Radio";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/radio" element={<Radio />} />
       </Routes>
     </div>
   );
