e4d74fd105fddd350ea1e214cf29e51fb5b15e5d
이수민 <judy3504@naver.com>
2024-03-20 11:09:06 +0900

    design: 나도예측하기 link 설정

--- front/src/components/Predict/PredictCreateForm.tsx
+++ front/src/components/Predict/PredictCreateForm.tsx
@@ -38,5 +38,5 @@ const Wrapper = styled.div`
   border-radius: 30px;
   margin-left: 300px;
   background-color: white;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.2);
 `;
--- front/src/pages/Predict/Predict.tsx
+++ front/src/pages/Predict/Predict.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 // 컴포넌트
 import Title from "../../components/Common/Title";
@@ -7,41 +8,43 @@ import Search from "../../components/Predict/Search";
 import Sort from "../../components/Predict/Sort";
 
 const buttonStyleDark = {
-    backgroundColor: "#1E3659",
-    color: "white",
-    border: "none",
-    width: "110px",
-    height: "40px",
-    borderRadius: "20px",
+  backgroundColor: "#1E3659",
+  color: "white",
+  border: "none",
+  width: "110px",
+  height: "40px",
+  borderRadius: "20px",
 };
 
 function Predict() {
-    return (
-        <Wrapper>
-            <FirstContainer>
-                <Title text="주식 예측 하기" />
-                <SquareBtn text="나도 예측하기" style={buttonStyleDark} />
-            </FirstContainer>
-            <SecondContainer>
-                <Search type="text" placeholder="종목명을 입력하세요." />
-                <Sort />
-            </SecondContainer>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <FirstContainer>
+        <Title text="주식 예측 하기" />
+        <Link to="/predict/create">
+          <SquareBtn text="나도 예측하기" style={buttonStyleDark} />
+        </Link>
+      </FirstContainer>
+      <SecondContainer>
+        <Search type="text" placeholder="종목명을 입력하세요." />
+        <Sort />
+      </SecondContainer>
+    </Wrapper>
+  );
 }
 
 export default Predict;
 
 const Wrapper = styled.div`
-    padding-left: 200px;
+  padding-left: 200px;
 `;
 
 const FirstContainer = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `;
 
 const SecondContainer = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
 `;
