f0779cabbc347ec181cf5f7b4a71054a094e8134
양성주 <ztjdwnz@naver.com>
2024-03-21 13:02:15 +0900

    feat: predict axios
    Change-Id: I3f73af7de4bf8ddedb30dbd6750e4cb3525e68d0

--- front/src/components/Predict/PredictList.tsx
+++ front/src/components/Predict/PredictList.tsx
@@ -1,127 +1,155 @@
 import styled from "styled-components";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { selectPredicts } from "../../apis/api/Predict";
+import { selectPredictsByStockName } from "../../apis/api/Predict";
+import { deletePredict } from "../../apis/api/Predict";
 
+import Deletebtn from "../../assets/img/button/Deletebtn.svg";
+import Reportbtn from "../../assets/img/button/Reportbtn.svg";
 import Feedbtn from "../../assets/img/button/Feedbtn.svg";
-
+import FeedOpenbtn from "../../assets/img/button/FeedOpenbtn.svg";
 
 interface FeedProps {
-  pdUpDown: boolean;
-  pdResult: string;
+    pdUpDown: boolean;
+    pdResult: string;
 }
 
+type PredictProps = {
+    sorted: string;
+};
 
-function PredictList() {
+function PredictList(props: PredictProps) {
+    const [data, setData] = useState<any[]>([]);
+    const [openItems, setOpenItems] = useState<number[]>([]);
 
-  const [openItems, setOpenItems] = useState<number[]>([]);
+    useEffect(() => {
+        // PredictList 컴포넌트가 마운트될 때 데이터를 가져옴
+        const fetchData = async () => {
+            try {
+                const result = await selectPredicts(props.sorted); // props로 전달된 sorted 값을 사용하여 데이터 가져옴
+                setData(result);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
 
-  const toggleContent = (pdNo: number) => {
-    setOpenItems(prevState => {
-      if (prevState.includes(pdNo)) {
-        return prevState.filter(item => item !== pdNo);
-      } else {
-        return [...prevState, pdNo];
-      }
-    });
-  };
+        fetchData();
+    }, [props.sorted]);
 
-  interface ClickProps {
-    isOpen: boolean;
-  }
-  
-  const Click = styled.div<ClickProps>`
-    display: ${props => props.isOpen ? 'flex' : 'none'};
-    padding: 10px 30px 30px 30px;
-  `;
+    const toggleContent = (pdNo: number) => {
+        setOpenItems((prevState) => {
+            if (prevState.includes(pdNo)) {
+                return prevState.filter((item) => item !== pdNo);
+            } else {
+                return [pdNo];
+            }
+        });
+    };
 
-  const header =  [
-    {
-      pdNo: 1,
-      stockName: '삼성전자',
-      userName: '다라란',
-      content: '근거입니당',
-      pdDate: '2024-03-20',
-      pdValue: '75000',
-      pdUpDown: true,
-      pdResult: ""
-    },
-    {
-      pdNo: 2,
-      stockName: 'SK하이닉스',
-      userName: '양나박이',
-      content: '근거입니당',
-      pdDate: '2024-03-20',
-      pdValue: '175000',
-      pdUpDown: false,
-      pdResult: ""
-    },
-    {
-      pdNo: 3,
-      stockName: '카카오',
-      userName: '안녕하세요제이름은',
-      content: '2차전지 관련 주로  현재 관련 주식이 평균 12% 이상 상승하였음 □□화성, 파이☆, ◇에너지, 자이○  등 현재 호재가 있음 ',
-      pdDate: '2024-03-20',
-      pdValue: '200',
-      pdUpDown: false,
-      pdResult: "상승"
-    },
-  ]
-  return (
-    <Wrapper>
-      {header.map((item) => (
-        <Feed key={item.pdNo} pdUpDown={item.pdUpDown} pdResult={item.pdResult}>
-        <Noncllick>
-          <p style={{ width: "100px" }}>{item.stockName}</p>
-          <p style={{ width: "100px" }}>{item.userName}</p>
-          <p style={{ width: "100px" }}>{item.pdDate}</p>
-          <p style={{ width: "100px" }}>{item.pdValue} {item.pdUpDown ? '▲' : '▼'}</p>
-          <button onClick={() => toggleContent(item.pdNo)}>
-              <img src={Feedbtn} alt="Feed Button" />
-            </button>
-          </Noncllick>
-          <Click isOpen={openItems.includes(item.pdNo)}>
-            {item.content}
-          </Click>
-        </Feed>
-      ))}
-    </Wrapper>
-  );
+    interface ClickProps {
+        isOpen: boolean;
+    }
+
+    const Click = styled.div<ClickProps>`
+        display: ${(props) => (props.isOpen ? "flex" : "none")};
+        padding: 10px 30px 30px 30px;
+        flex-direction: column;
+    `;
+
+    return (
+        <Wrapper>
+            {data.map((item) => (
+                <Feed
+                    key={item.pdNo}
+                    pdUpDown={item.pdUpDown}
+                    pdResult={item.pdResult}
+                >
+                    <Noncllick>
+                        <p style={{ width: "100px" }}>{item.stockName}</p>
+                        <p style={{ width: "100px" }}>{item.userName}</p>
+                        <p style={{ width: "100px" }}>{item.pdDate}</p>
+                        <p style={{ width: "100px" }}>
+                            {item.pdValue}
+                            <span
+                                style={{
+                                    color: item.pdUpDown ? "red" : "blue",
+                                    marginLeft: "5px",
+                                }}
+                            >
+                                {item.pdUpDown ? "▲" : "▼"}
+                            </span>
+                        </p>
+                        <button onClick={() => toggleContent(item.pdNo)}>
+                            <img
+                                src={
+                                    openItems.includes(item.pdNo)
+                                        ? FeedOpenbtn
+                                        : Feedbtn
+                                }
+                                alt="Feed Button"
+                            />
+                        </button>
+                    </Noncllick>
+                    <Click isOpen={openItems.includes(item.pdNo)}>
+                        {item.pdContent}
+                        <FeedIcon>
+                            <img
+                                src={Deletebtn}
+                                alt="삭제"
+                                style={{ marginRight: "20px" }}
+                                onClick={() => deletePredict(item.pdNo)}
+                            />
+                            <img src={Reportbtn} alt="신고" />
+                        </FeedIcon>
+                    </Click>
+                </Feed>
+            ))}
+        </Wrapper>
+    );
 }
 
 export default PredictList;
-const Wrapper = styled.div`
-
-`;
+const Wrapper = styled.div``;
 
 const Feed = styled.div<FeedProps>`
+    border-radius: 15px;
+    background-color: white;
+    box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.2);
+    margin-bottom: 20px;
+    background: ${(props) =>
+        props.pdResult
+            ? "linear-gradient(to bottom, #F0F0F0, #FFFFFF)"
+            : props.pdUpDown
+            ? "linear-gradient(to bottom, #FFF3F3, #FFFFFF)"
+            : "linear-gradient(to bottom, #F3FAFF, #FFFFFF)"};
 
-border-radius: 15px;
-background-color: white;
-box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.2);
-margin-bottom: 20px;
-background: ${props => props.pdResult ? 'linear-gradient(to bottom, #F0F0F0, #FFFFFF)'
-: (props.pdUpDown ? 'linear-gradient(to bottom, #FFF3F3, #FFFFFF)'
-: 'linear-gradient(to bottom, #F3FAFF, #FFFFFF)')};
+    button {
+        background: none;
+        border: none;
+        cursor: pointer;
+        outline: none;
+    }
 
-
-button {
-  background: none;
-  border: none;
-  cursor: pointer;
-  outline: none;
-}
-
-img {
-  width: 24px;
-  height: 24px;
-}
+    img {
+        width: 24px;
+        height: 24px;
+    }
 `;
 
 const Noncllick = styled.div`
-display: flex;
-justify-content: space-evenly;
-p{
-  width: "100px";
-  text-align: center;
-}
+    display: flex;
+    justify-content: space-evenly;
+    p {
+        width: "100px";
+        text-align: center;
+    }
 `;
 
+const FeedIcon = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    padding-right: 10px;
+    img{
+      cursor: pointer;
+    }
+`;
