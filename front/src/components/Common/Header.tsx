60595315c0e0fb4cd877f1f4925cd9a2559ff8fb
이수민 <judy3504@naver.com>
2024-03-21 09:46:21 +0900

    design: 메인화면 flow bar 디자인

--- front/src/components/Common/Header.tsx
+++ front/src/components/Common/Header.tsx
@@ -8,7 +8,6 @@ function Header() {
       <ImgContainer>
         <img src={Logo} alt="logo" />
       </ImgContainer>
-      <AlarmContainer></AlarmContainer>
     </div>
   );
 }
@@ -17,16 +16,10 @@ export default Header;
 
 const ImgContainer = styled.div`
   img {
-    width: 100px;
-    margin-top: 10px;
+    width: 150px;
+    margin-top: 30px;
   }
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const AlarmContainer = styled.div`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
 `;
--- front/src/components/Common/SideNavBar.tsx
+++ front/src/components/Common/SideNavBar.tsx
@@ -55,7 +55,7 @@ const Side = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
-  padding-top: 20px;
+  padding-top: 40px;
   width: 100px;
 `;
 
