fd39118e818a7e1430854b39863a92b696992a82
이수민 <judy3504@naver.com>
2024-03-21 10:24:08 +0900

    feat: 3d모델 사용자  비활성화

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -22,8 +22,7 @@ import Predict from "./pages/Predict/Predict";
 import PredictCreate from "./pages/Predict/PredictCreate";
 import Intro from "./components/Landing/Intro";
 import styled from "styled-components";
-import Hippo3d from "./components/Character/Hippo3d";
-import Unicorn3d from "./components/Character/Unicorn3d";
+import Bear3d from "./components/Character/Bear3d";
 
 function App() {
   const location = useLocation();
@@ -57,7 +56,7 @@ function App() {
           <Route path="/radio" element={<Radio />} />
           <Route path="/main" element={<Main />} />
           <Route path="/my-page" element={<Mypage />} />
-          <Route path="/3d" element={<Unicorn3d />} />
+          <Route path="/3d" element={<Bear3d />} />
 
           <Route path="/zbti" element={<ZbtiStart />} />
           <Route path="/problem1" element={<Problem1 />} />
