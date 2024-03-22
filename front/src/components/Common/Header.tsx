fd8986c4b2817d552d8caa57e5241d3a289ee270
이수민 <judy3504@naver.com>
2024-03-22 15:29:46 +0900

    design: header 수정
    Change-Id: Ia041a003450f7ae029f5866848016350f368a4e9

--- front/src/components/Common/Header.tsx
+++ front/src/components/Common/Header.tsx
@@ -1,24 +1,75 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 import Logo from "../../assets/img/Logo.svg";
+import alarmbtn from "../../assets/img/button/Alarmbtn.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <ImgContainer>
-      <img src={Logo} alt="logo" />
-    </ImgContainer>
+    <Container>
+      <ImgContainer>
+        <img src={Logo} alt="logo" className="logo" />
+      </ImgContainer>
+      <LoginContainer>
+        <Link to="/login" style={{ textDecoration: "none", color: "black" }}>
+          <div>로그인</div>
+        </Link>
+        <CircleContainer>
+          <Circle>
+            <img src={alarmbtn} alt="alarmbtn" />
+          </Circle>
+        </CircleContainer>
+      </LoginContainer>
+    </Container>
   );
 }
 
 export default Header;
 
-const ImgContainer = styled.div`
-  img {
+const Container = styled.div`
+  .logo {
     width: 150px;
-    margin: 30px 0;
+    margin: 30px 490px;
   }
   display: flex;
   justify-content: center;
   align-items: center;
-  padding-top: 10px;
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const LoginContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const CircleContainer = styled.div`
+  margin-top: 50px;
+  margin-left: 10px;
+`;
+
+const CircleMotion = keyframes`
+`;
+
+const Circle = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #2a4263;
+  position: relative;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  &:hover {
+    // animation: ${CircleMotion} 1s infinite alternate;
+  }
 `;
