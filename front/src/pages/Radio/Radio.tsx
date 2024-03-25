7ebae4a4548316b262cf8914aa902b63b3ea7b37
s_971001 <s_971001@naver.com>
2024-03-25 23:10:54 +0900

    design: character크기

--- front/src/pages/Radio/Radio.tsx
+++ front/src/pages/Radio/Radio.tsx
@@ -55,6 +55,10 @@ function Player() {
     }
   } 
 
+  const subtitle = () => {
+
+  }
+
   return (
     <Wrapper>
       <Title text="뉴스를 들려줄게요" />
@@ -63,7 +67,7 @@ function Player() {
         <ImgBtn src={Playicon} onClick={clickBtn} disabled={isPaused? false:true} style={imgBtnStyle}></ImgBtn>
         <ImgBtn src={Pauseicon} onClick={clickBtn} disabled={isPaused? true:false} style={imgBtnStyle}></ImgBtn>
       </PlayContainer>
-      <Character3d name="Cow" characterScale={1} canvasWidth={700} canvasHeight={400} />
+      <Character3d name="Bear" characterScale={0.53} canvasWidth={400} canvasHeight={200} />
       <SquareBtn text="자막보기" style={buttonStyleDark} />
     </Wrapper>
   )
