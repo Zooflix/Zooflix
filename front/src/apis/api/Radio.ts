e8de5394e05a5119a649b343909a3a03ed5078fc
s_971001 <s_971001@naver.com>
2024-03-25 17:25:55 +0900

    feat: tts 재생/중단

--- front/src/apis/api/Radio.ts
+++ front/src/apis/api/Radio.ts
@@ -1,21 +1,17 @@
 import { axios } from "../utils/axios";
 
-const REST_RADIO_API = "http://localhost:8089/radio";
+const REST_RADIO_API = "http://localhost:8089";
 
 // 라디오
 export async function playRadio() {
     try {
-        const response = await axios.post(`${REST_RADIO_API}`, {
-            responseType: 'arraybuffer',
-            headers: {
-            'Content-Type': 'application/json',
-            },
+        const response = await axios.get(`${REST_RADIO_API}/radio`, {
+            responseType: 'blob',
         });
-        const audioData = response.data;
-        const audioBlob = new Blob([audioData], { type: 'audio/mpeg'});
-        const audioUrl = URL.createObjectURL(audioBlob);
-        return audioBlob;
-    } catch (e) {
-        console.log(e);
+        const url = URL.createObjectURL(response.data);
+        return url;
+    } catch (error) {
+        console.error('Error while fetching radio:', error);
+        throw error;
     }
 }
