bcb613112cb8f6a9c960972370553be3713ec26e
조민준 <cjmg1085@naver.com>
2024-03-26 11:01:14 +0900

    docs: recommit

--- back/src/main/java/com/zooflix/be_zooflix/domain/myPage/dto/response/MyInfoDto.java
+++ back/src/main/java/com/zooflix/be_zooflix/domain/myPage/dto/response/MyInfoDto.java
@@ -7,11 +7,12 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MyInfoDto {
+    private int userNo;                 //유저 인덱스
     private String userName;            //유저 닉네임
     private double userTemperature;     //유저 온도
     private int predictCount;           //총 예측 횟수
     private int successCount;           //예측 성공 횟수
-    private double predictionRate;         //예측 비율
+    private double predictionRate;      //예측 비율
     private int SubscribeFromMe;        //내가 구독한 수
     private int subscribeToMe;          //나를 구독한 수
 
