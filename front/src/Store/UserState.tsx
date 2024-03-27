f4ef97d81501571381697b161ae728afed5271cb
이수민 <judy3504@naver.com>
2024-03-27 15:29:33 +0900

    feat: alarm axios 연동중
    Change-Id: Idb7d60ceea2346f92bbe3cce40eda111a0494bfd

--- front/src/Store/UserState.tsx
+++ front/src/Store/UserState.tsx
@@ -1,13 +1,19 @@
-import { atom } from 'recoil';
- 
+import { atom } from "recoil";
+
 // 아이디
 export const userIdState = atom({
-    key: 'userIdState',
-    default: '',
-  });
-  
+  key: "userIdState",
+  default: "",
+});
+
 // 비번
 export const userPwState = atom({
-    key: 'userPwState',
-    default: '',
+  key: "userPwState",
+  default: "",
+});
+
+//userNo
+export const userNoState = atom({
+  key: "userNoState",
+  default: 0,
 });
