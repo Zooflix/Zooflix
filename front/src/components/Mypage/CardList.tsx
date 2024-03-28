36daba3fc6c2e932f1c0a84ead1d19d82f3d1aa5
이수민 <judy3504@naver.com>
2024-03-28 14:05:17 +0900

    design: my-page 패딩찾기
    Change-Id: Ie42bed62664530195265d0152347a40afbc26422

--- front/src/components/Mypage/CardList.tsx
+++ front/src/components/Mypage/CardList.tsx
@@ -1,44 +1,44 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import Card from "./Card";
 import { useRecoilState } from "recoil";
 import { stockSubListState } from "../../Store/StockSubscribeState";
 
 const SubscribeStockList: React.FC = () => {
-    const [myStockList, setMyStockList] = useRecoilState(stockSubListState);
+  const [myStockList, setMyStockList] = useRecoilState(stockSubListState);
 
-    if (!myStockList || myStockList.length === 0) {
-        return <NoResultsMessage>결과를 찾을 수 없습니다</NoResultsMessage>;
-    }
+  if (!myStockList || myStockList.length === 0) {
+    return <NoResultsMessage>결과를 찾을 수 없습니다</NoResultsMessage>;
+  }
 
-    return (
-        <Wrapper>
-            <Container>
-                {myStockList.map((card, index) => (
-                    <Card key={index} card={card} />
-                ))}
-            </Container>
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <Container>
+        {myStockList.map((card, index) => (
+          <Card key={index} card={card} />
+        ))}
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default SubscribeStockList;
 const Wrapper = styled.div``;
 const NoResultsMessage = styled.div`
-    color: black;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 `;
 
 const Container = styled.div`
-    display: flex;
-    overflow-y: auto;
-    flex-direction: column;
-    overflow-y: auto;
-    scrollbar-width: none;
-    &::-webkit-scrollbar {
-        display: none;
-    }
+  display: flex;
+  overflow-y: auto;
+  flex-direction: column;
+  overflow-y: auto;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
--- front/src/components/Mypage/MySubscribeList.tsx
+++ front/src/components/Mypage/MySubscribeList.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import styled from "styled-components";
 import MySubscription from "./MySubscription";
 import { useRecoilState } from "recoil";
@@ -6,80 +5,79 @@ import { myPageSubscribeListState } from "../../Store/MyPageState";
 import CardList from "./CardList";
 
 function MySubscribeList() {
-    const [myPageSubscribeList, setMyPageSubScribeList] = useRecoilState(
-        myPageSubscribeListState
-    );
+  const [myPageSubscribeList, setMyPageSubScribeList] = useRecoilState(
+    myPageSubscribeListState
+  );
 
-    const deleteSubscription = (subscribeNo: number) => {
-        setMyPageSubScribeList(
-            myPageSubscribeList.filter(
-                (subscribe) => subscribe.subscribeNo !== subscribeNo
-            )
-        );
-    };
-
-    return (
-        <Wrapper>
-            <Container>
-                <LeftSide>
-                    내가 정기 구독 중인 주식
-                    <CardSection>
-                        <CardList/>
-                    </CardSection>
-                </LeftSide>
-                <RightSide>
-                    <div>내가 구독 중인 회원</div>
-                    <div>
-                        {myPageSubscribeList &&
-                            myPageSubscribeList.map((subscribe) => (
-                                <MySubscription
-                                    key={subscribe.subscribeNo}
-                                    onSubscribe={subscribe}
-                                    onDelete={deleteSubscription}
-                                />
-                            ))}
-                    </div>
-                </RightSide>
-            </Container>
-        </Wrapper>
+  const deleteSubscription = (subscribeNo: number) => {
+    setMyPageSubScribeList(
+      myPageSubscribeList.filter(
+        (subscribe) => subscribe.subscribeNo !== subscribeNo
+      )
     );
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <LeftSide>
+          내가 정기 구독 중인 주식
+          <CardSection>
+            <CardList />
+          </CardSection>
+        </LeftSide>
+        <RightSide>
+          <div>내가 구독 중인 회원</div>
+          <div>
+            {myPageSubscribeList &&
+              myPageSubscribeList.map((subscribe) => (
+                <MySubscription
+                  key={subscribe.subscribeNo}
+                  onSubscribe={subscribe}
+                  onDelete={deleteSubscription}
+                />
+              ))}
+          </div>
+        </RightSide>
+      </Container>
+    </Wrapper>
+  );
 }
 
 export default MySubscribeList;
 
 const Wrapper = styled.div`
-    margin: 10px;
-    background: #ffffff;
-    border: 0.917219px solid #e7e7e7;
-    box-shadow: 3.1002px 3.1002px 15.4918px -7.7505px rgba(0, 0, 0, 0.4);
-    border-radius: 12.8411px;
-    overflow: auto;
-    max-height: 580px;
-    scrollbar-color: #a5a5a5;
-    scrollbar-width: thin;
+  //   margin: 10px;
+  background: #ffffff;
+  border: 0.917219px solid #e7e7e7;
+  box-shadow: 3.1002px 3.1002px 15.4918px -7.7505px rgba(0, 0, 0, 0.4);
+  border-radius: 12.8411px;
+  overflow: auto;
+  max-height: 580px;
+  scrollbar-color: #a5a5a5;
+  scrollbar-width: thin;
 `;
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    // align-items: center;
-    text-align: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  text-align: center;
 `;
 
 const LeftSide = styled.div`
-    width: 235px;
-    margin: 20px 52px;
-    flex-direction: column;
+  //   width: 235px;
+  //   margin: 20px 52px;
+  flex-direction: column;
 `;
 
 const CardSection = styled.div`
-    width: 190px;
-    margin: 20px;
+  width: 190px;
+  //   margin: 20px;
 `;
 
 const RightSide = styled.div`
-    width: 330px;
-    margin: 20px 52px;
-    flex-direction: column;
+  width: 330px;
+  //   margin: 20px 52px;
+  flex-direction: column;
 `;
