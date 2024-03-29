db09f6ee8d08ca6f69179f66b95d30b69ff5d248
조민준 <cjmg1085@naver.com>
2024-03-29 16:19:55 +0900

    docs:update UserDetailModel

--- front/src/components/Mypage/MyInfo.tsx
+++ front/src/components/Mypage/MyInfo.tsx
@@ -1,55 +1,62 @@
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import {
-  myPageInfoState,
-  myPagePredictListState,
+    myPageInfoState,
+    myPagePredictListState,
 } from "../../Store/MyPageState";
+import { useState } from "react";
 
 function MyInfo() {
-  const [myPageInfo, setMyPageInfo] = useRecoilState(myPageInfoState);
-  const [myPagePredictList, setMyPagePredictList] = useRecoilState(
-    myPagePredictListState
-  );
+    const [myPageInfo, setMyPageInfo] = useRecoilState(myPageInfoState);
+    const [myPagePredictList, setMyPagePredictList] = useRecoilState(
+        myPagePredictListState
+    );
 
-  let successCnt = 0; //성공 횟수
+    let successCnt = 0; //성공 횟수
+    let successRate = 0;  //성공 확률
 
-  myPagePredictList.forEach((item) => {
-    if (item.pdResult === "성공") {
-      successCnt += 1;
-    }
-  });
 
-  // 소수점 둘째 짜리까지 성공 비율 -> rate
-  let rateOfPredict = Math.round(successCnt / myPagePredictList.length);
-  let rate = Math.round(rateOfPredict * 100) / 100;
+    myPagePredictList.forEach((item) => {
+        if (item.pdResult === "성공") {
+            successCnt += 1;
+        }
+    });
 
-  return (
-    <Wrapper>
-      <LeftsideQuestion>
-        <h4>총 예측 횟수</h4>
-        <h4>예측 성공 횟수</h4>
-        <h4>예측률</h4>
-        <h4>구독</h4>
-        <h4>구독자</h4>
-      </LeftsideQuestion>
-      <RightSideAnswer>
-        <h4>{myPagePredictList.length}</h4>
-        <h4>{successCnt}</h4>
-        <h4>{rate}%</h4>
-        <h4>{myPageInfo.subscribeFromMe}</h4>
-        <h4>{myPageInfo.subscribeToMe}</h4>
-      </RightSideAnswer>
-    </Wrapper>
-  );
+    // 소수점 둘째 짜리까지 성공 비율 -> rate
+    let rateOfPredict = Math.round(
+      successCnt / myPagePredictList.length * 100
+    );
+
+    let rate = Math.round(rateOfPredict * 100) / 100;
+    successRate = rate;
+
+    return (
+        <Wrapper>
+            <LeftsideQuestion>
+                <h4>총 예측 횟수</h4>
+                <h4>예측 성공 횟수</h4>
+                <h4>예측률</h4>
+                <h4>구독</h4>
+                <h4>구독자</h4>
+            </LeftsideQuestion>
+            <RightSideAnswer>
+                <h4>{myPagePredictList.length}</h4>
+                <h4>{successCnt}</h4>
+                <h4>{successRate}%</h4>
+                <h4>{myPageInfo.subscribeFromMe}</h4>
+                <h4>{myPageInfo.subscribeToMe}</h4>
+            </RightSideAnswer>
+        </Wrapper>
+    );
 }
 
 export default MyInfo;
 
 const Wrapper = styled.div`
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  padding: 5px 70px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    padding: 5px 70px;
 `;
 
 const LeftsideQuestion = styled.div``;
