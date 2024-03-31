dfc2612461ad5c9f90b8556fb5ba23d3a99d8349
이수민 <judy3504@naver.com>
2024-03-31 19:42:19 +0900

    design: header수정,,
    Change-Id: I5cd5441d8de4f1f6f08f47c4615abb63dc8476a5

--- front/src/components/Mypage/CardList.tsx
+++ front/src/components/Mypage/CardList.tsx
@@ -8,30 +8,29 @@ interface Props {
 
 function CardList({ myStockList }: Props) {
   return (
-    <Wrapper>
-      <Container>
-        {myStockList.map((card, index) => (
-          <Card key={index} card={card} />
-        ))}
-      </Container>
-    </Wrapper>
+    // <Wrapper>
+    <Container>
+      {myStockList.map((card, index) => (
+        <Card key={index} card={card} />
+      ))}
+    </Container>
+    // </Wrapper>
   );
 }
 
 export default CardList;
-const Wrapper = styled.div`
-  width: 1000px;
-  border: none;
-  border-radius: 30px;
-  background-color: white;
-  box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.2);
-  padding: 30px 30px 10px 30px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 100px;
-`;
+// const Wrapper = styled.div`
+//   border: none;
+//   border-radius: 30px;
+//   background-color: white;
+//   box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.2);
+//   padding: 30px 30px 10px 30px;
+//   display: flex;
+//   flex-direction: column;
+//   justify-content: center;
+//   align-items: center;
+//   min-height: 100px;
+// `;
 
 const Container = styled.div`
   display: flex;
