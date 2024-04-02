9d144e9d9785e6cab9a3f706430b8ba2f2a74b05
김진우 <lkk7209@naver.com>
2024-04-02 23:53:44 +0900

    fix: jwt 예외처리

--- front/src/apis/utils/jwt.ts
+++ front/src/apis/utils/jwt.ts
@@ -1,23 +1,35 @@
 export function getJwtUserNo() {
   const token = localStorage.getItem("access");
+  if (!token) {
+    return 0;
+  }
   const decoded = require("jwt-decode").jwtDecode(token);
   return decoded.userNo;
 }
 
 export function getJwtUserId() {
   const token = localStorage.getItem("access");
+  if (!token) {
+    return "";
+  }
   const decoded = require("jwt-decode").jwtDecode(token);
   return decoded.userId;
 }
 
 export function getJwtUserName() {
   const token = localStorage.getItem("access");
+  if (!token) {
+    return "";
+  }
   const decoded = require("jwt-decode").jwtDecode(token);
   return decoded.userName;
 }
 
 export function getJwtUserZbti() {
   const token = localStorage.getItem("access");
+  if (!token) {
+    return "Bear";
+  }
   const decoded = require("jwt-decode").jwtDecode(token);
   return decoded.userZbti;
 }
