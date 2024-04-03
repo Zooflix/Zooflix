180ad8c5d60b70b432753c9e66e4ea2efd75f3eb
s_971001 <s_971001@naver.com>
2024-04-03 17:49:01 +0900

    fix: sonarqube code

--- front/src/pages/Zbti/Problem2.tsx
+++ front/src/pages/Zbti/Problem2.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
 import { useRecoilState } from "recoil";
 import { zbtiQuestionState } from "../../Store/ZbtiState";
 
